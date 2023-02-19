a373351dbd58f3c46c1af079367849de6e8a2cce
jsterner30 <jts234@byu.edu>
2023-02-18 23:38:14 -0700

    main updates

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import login from "./login.js";
 import create from "./create.js";
+import newPost from "./newPost.js";
+import feed from "./feed.js";
+import logout from "./logout.js";
+import isLogged from "./isLogged.js";
+import load from "./load.js";
 
+window.load = load;
+window.isLogged = isLogged;
+window.logout = logout;
+window.feed = feed;
 window.login = login;
 window.create = create;
+window.newPost = newPost;
